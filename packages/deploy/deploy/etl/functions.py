6a6e5e02152a7e0e6e2d2e1d54fe5e988569fddb
Paul Cowan <paul.cowan@cutting.scot>
2026-04-05 10:50:33 +0100

    chore: optimize Docker builds by excluding unnecessary files and cleaning CDK output before deployment

--- packages/deploy/deploy/etl/functions.py
+++ packages/deploy/deploy/etl/functions.py
@@ -26,6 +26,7 @@ class EtlFunctions(Construct):
         self.predictor = aws_lambda.DockerImageFunction(self, "PredictorFunction",
             code=aws_lambda.DockerImageCode.from_image_asset(
                 str(REPO_DIR / "packages" / "ml" / "src"),
+                exclude=["__pycache__", ".venv"],
             ),
             memory_size=1024,
             tracing=aws_lambda.Tracing.ACTIVE,
