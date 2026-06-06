2e1f7844f354efee702094ccdc743327db841dd3
Paul Cowan <paul.cowan@cutting.scot>
2026-06-06 14:08:11 +0100

    chore: add CloudFront prefix list lookup and ALB security group with ingress rule for CloudFront origin-facing traffic on port 80 in WebsiteStack, update CDK context with VPC and prefix list lookups for us-east-1

--- packages/deploy/deploy/website_stack/website_stack.py
+++ packages/deploy/deploy/website_stack/website_stack.py
@@ -9,3 +9,13 @@ class WebsiteStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
         
         self.vpc = ec2.Vpc.from_lookup(self, "DefaultVpc", is_default=True)
+
+        cloudfront_prefix = ec2.PrefixList.from_lookup(self, "CloudFrontPrefix",
+            prefix_list_name="com.amazonaws.global.cloudfront.origin-facing",
+        )
+
+        alb_sg = ec2.SecurityGroup(self, "AlbSg", vpc=self.vpc)
+        alb_sg.add_ingress_rule(
+            peer=ec2.Peer.prefix_list(cloudfront_prefix.prefix_list_id),
+            connection=ec2.Port.tcp(80),
+        )
