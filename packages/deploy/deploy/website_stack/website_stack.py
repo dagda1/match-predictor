5375f7e713e90261ea5cf4a9033b2241cd0c9e28
Paul Cowan <paul.cowan@cutting.scot>
2026-06-06 15:05:10 +0100

    chore: extract ALB and network infrastructure into separate constructs, add ApplicationLoadBalancer with HTTP listener and target group for port 3000, update CDK context with CloudFront prefix list lookup for us-east-1

--- packages/deploy/deploy/website_stack/website_stack.py
+++ packages/deploy/deploy/website_stack/website_stack.py
@@ -1,21 +1,16 @@
 from aws_cdk import (
     Stack,
-    aws_ec2 as ec2,
 )
 from constructs import Construct
+from deploy.website_stack.network import Network
+from deploy.website_stack.alb import Alb
 
 class WebsiteStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
         
-        self.vpc = ec2.Vpc.from_lookup(self, "DefaultVpc", is_default=True)
+        network = Network(self, "Network")
+        alb = Alb(self, "Alb", vpc=network.vpc, alb_sg=network.alb_sg)
 
-        cloudfront_prefix = ec2.PrefixList.from_lookup(self, "CloudFrontPrefix",
-            prefix_list_name="com.amazonaws.global.cloudfront.origin-facing",
-        )
 
-        alb_sg = ec2.SecurityGroup(self, "AlbSg", vpc=self.vpc)
-        alb_sg.add_ingress_rule(
-            peer=ec2.Peer.prefix_list(cloudfront_prefix.prefix_list_id),
-            connection=ec2.Port.tcp(80),
-        )
+        
